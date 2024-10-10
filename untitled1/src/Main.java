198747b88739ccdc41e46285b10378dd5cc89a83
bassem-ashraf331 <basem.saad.2025@aiu.edu.eg>
2024-10-10 20:42:26 +0300

    new task

---
+++ untitled1/src/Main.java
@@ -0,0 +1,23 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        //question 5
+
+        System.out.println("  *                          *");
+        System.out.println("  **                        **");
+        System.out.println("  ***                      ***");
+        System.out.println("  ****                    ****");
+        System.out.println("  *****                  *****");
+
+       //question 6
+
+      System.out.println("6*3-9/4="+(6*3-9/4));
+      System.out.println("6+3*9/4="+(6+3*9/4));
+      System.out.println("Result of (6+(18%(17-12))): " + (6 + (18 %(17-12))));
+      System.out.print("7/3*1.2+3/2="+(7/3*1.2+3/2));
+      System.out.println();
+      System.out.println("2.0*2.4+2.25*4.0/2.0="+(2.0*2.4+2.25*4.0/2.0));
+    }
+}
+
